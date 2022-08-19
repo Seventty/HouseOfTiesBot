8525d7954aaf6f6d6262fc9196bf065f03037cf4
seventty <elmastigeron2015@gmail.com>
2022-08-19 12:41:20 -0400

    update

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,2 @@
 export { default as start } from './start/start';
-export { default as heya } from './heya/heya';
-export { default as say } from './say/say';
-export { default as rsay } from './reverseSay/rsay';
-export { default as fsay } from './fancySay/fsay';
+
